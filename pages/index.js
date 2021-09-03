4ca0c25db8193573f27b4815156fe7a8be72f859
Andres B <andres.bustos@globant.com>
2021-09-03 17:16:11 -0500

    demostration changes on live

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Mujeres Poderosas</h1>
+      <p>Esta es una pagina para mujeres, si eres hombre vete</p>
       <Link href="/courses">
         <a>To cursos..</a>
       </Link>
