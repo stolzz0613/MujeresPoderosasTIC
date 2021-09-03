38fd43d4227a309d854444ac6a3b93e536790495
Andres B <andres0613@utp.edu.co>
2021-09-03 00:59:44 -0500

    03/09/2021: Ejemplo Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 function Home() {
   return (
     <div>
       <h1>Mujeres Poderosas</h1>
+      <Link href="/courses">
+        <a>To cursos..</a>
+      </Link>
     </div>
   )
 }
