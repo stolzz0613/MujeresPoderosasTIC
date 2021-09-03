b9a9ea90452e4d14aac19feb60a4077e47b3fe55
Tatianag2 <tatiana3g2@gmail.com>
2021-09-03 18:11:13 -0500

    Prueba

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <h1>Mujeres Poderosas</h1>
-      <h3>Cambio por Tatiana</h3>
+      <h3>Cambio por Tatiana Garcia</h3>
       <p>Esta es una pagina para mujeres, si eres hombre vete</p>
       <Link href="/courses">
         <a>To cursos..</a>
