6b6570bc31a5cc9c7c86d48bd1c007ed4453ce2c
Andres B <andres.bustos@globant.com>
2021-09-11 20:41:36 -0500

    Checking production config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 const path = require('path')
 
-module.exports = {
-  assetPrefix: './',
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-}
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/MujeresPoderosasTIC/',
+  }],
+  {
+    basePath: '/MujeresPoderosasTIC',
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')]
+    }
+  },
+]);
