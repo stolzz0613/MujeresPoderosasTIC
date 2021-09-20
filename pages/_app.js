988660943fbd279d8b9a8c994e648b5cd92b6062
Andres B <andres.bustos@globant.com>
2021-09-19 19:48:49 -0500

    Correcciones sprint1y2

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import '../styles/globals.scss';
 import Header from '../components/Header';
 import { AppWrapper } from '../context/globalState';
 import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </AppWrapper>
   )
 }
