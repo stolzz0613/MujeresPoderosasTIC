3d64a77bad70266f0b0855ab88f1017f92e71774
Tatianag2 <tatiana3g2@gmail.com>
2021-11-23 18:02:05 -0500

    img header de cada pag

--- pages/news/index.js
+++ pages/news/index.js
@@ -17,6 +17,9 @@ function index() {
 
     return (
         <div className={newsStyle.container}>
+            <div className={newsStyle.containerImg}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path  fillOpacity="1" d="M0,192L80,181.3C160,171,320,149,480,160C640,171,800,213,960,208C1120,203,1280,149,1360,122.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            </div>
             <h1>Noticias</h1>
             <div className={newsStyle.container_cards}>
                 {news.map((e,i) => <Card key={i} data={e}/>)}
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -53,6 +53,9 @@ function index()  {
 
     return (
         <div className={profile.container}>
+            <div className={profile.containerImg}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,192L80,181.3C160,171,320,149,480,160C640,171,800,213,960,208C1120,203,1280,149,1360,122.7L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+            </div>
             { !store.userLogged.logged
                 ? <h1>Acceso Restringido</h1>
                 : <>
